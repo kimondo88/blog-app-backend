a66b9286ed765da5e055012a7e2e982e88357ffb
kimondo88 <kimondo1988@o2.pl>
2022-04-01 13:58:15 +0200

    add initial commit, also for vercel.

---
+++ README.md
@@ -0,0 +1,10 @@
+Blog App Backend API
+
+# Technologies that will be used used in project.
+
+- Express
+
+# Documentation
+
+- It is available in docs. Rough draft. 
+- It will be automated from jsdoc notation. 
---
+++ api/index.js
@@ -0,0 +1,9 @@
+const app = require('express')();
+const { v4 } = require('uuid');
+
+app.get('/api', (req, res)=>{
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+});
+
+module.exports = app; 
