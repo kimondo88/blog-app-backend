1d5f67cd58a7a991506831dae0e736762dfe2e4c
kimondo88 <kimondo1988@o2.pl>
2022-04-04 15:16:31 +0200

    add response to api route, also it work in vercel locally

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 const app = require('express')();
-const { v4 } = require('uuid');
+//const { v4 } = require('uuid');
 
 app.get('/api', (req, res)=>{
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.json({"test": 'testing purposes'})
 });
 
 module.exports = app; 
